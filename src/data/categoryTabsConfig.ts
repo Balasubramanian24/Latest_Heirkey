c4310df68647430ce57e99d547a057ccf57be390
Balasubramanian24 <spalanibala46@gmail.com>
2025-05-16 08:58:08 +0530

    Json files added to next category

--- src/data/categoryTabsConfig.ts
+++ src/data/categoryTabsConfig.ts
@@ -16,4 +16,18 @@ export const categoryTabsConfig = {
     { label: 'Notifications', path: '/category/funeralarrangements/notification' },
     { label: 'Proceedings', path: '/category/funeralarrangements/proceedings' },
   ],
+  importantcontacts: [
+    { label: 'Family', path: '/category/importantcontacts/familyfriends' },
+    { label: 'Work', path: '/category/importantcontacts/work' },
+    { label: 'Religious Affiliation', path: '/category/importantcontacts/religiousaffiliation' },
+    { label: 'Clubs', path: '/category/importantcontacts/clubs' },
+  ],
+  socialmedia: [
+    { label: 'Email', path: '/category/socialmedia/email' },
+    { label: 'Facebook', path: '/category/socialmedia/facebook' },
+    { label: 'Instagram', path: '/category/socialmedia/instagram' },
+    { label: 'Other Social Media', path: '/category/socialmedia/othersocialmedia' },
+    { label: 'Cell Phone', path: '/category/socialmedia/cellphone' },
+  ],
+  
 }; 
